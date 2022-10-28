4ab5031858241b20b8deaa0cca815d59ca3e1935
ITprofile00 <antoniofilippelli0@gmail.com>
2022-10-28 16:36:36 +0200

    Commit iniziale

---
+++ ProvaINGSW28102022/src/main/java/unical/ingsw/it/ProvaINGSW28102022/App.java
@@ -0,0 +1,13 @@
+package unical.ingsw.it.ProvaINGSW28102022;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ProvaINGSW28102022/src/test/java/unical/ingsw/it/ProvaINGSW28102022/AppTest.java
@@ -0,0 +1,20 @@
+package unical.ingsw.it.ProvaINGSW28102022;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
