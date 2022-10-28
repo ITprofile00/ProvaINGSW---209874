2ac4dd862af6d63fbce1235f52a9e3d8a9e2bb5f
ITprofile00 <antoniofilippelli0@gmail.com>
2022-10-28 16:51:50 +0200

    commit stringToInt

--- ProvaINGSW28102022/src/main/java/unical/ingsw/it/ProvaINGSW28102022/FunnyAlgorithms.java
+++ ProvaINGSW28102022/src/main/java/unical/ingsw/it/ProvaINGSW28102022/FunnyAlgorithms.java
@@ -70,7 +70,18 @@ public class FunnyAlgorithms {
 	 * @throws UnsupportedOperationException
 	 */
 	public int stringToIntConverter(String number) throws UnsupportedOperationException {
-		throw new UnsupportedOperationException("To be implemented");
+		//throw new UnsupportedOperationException("To be implemented");
+		
+		int n = Integer.parseInt(number);
+		
+		if (n < -32768 || n > 32768) {
+			throw new IllegalArgumentException("Range non rispettato");
+		}
+		
+		if(n!=0) {
+			throw new IllegalArgumentException("Errore! Numeri reali non ammessi!");
+		}
+		return n;
 	}
 
 }
